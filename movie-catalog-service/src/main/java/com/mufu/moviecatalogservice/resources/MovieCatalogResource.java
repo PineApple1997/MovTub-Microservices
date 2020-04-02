7856917a54d7e89890a048f6bb720d913584b592
hanmufu <hanmufu@gmail.com>
2020-04-01 23:42:25 -0400

    use RestTemplate to call Movie Info API

--- movie-catalog-service/src/main/java/com/mufu/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/mufu/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,12 +1,17 @@
 package com.mufu.moviecatalogservice.resources;
 
 import com.mufu.moviecatalogservice.models.CatalogItem;
+import com.mufu.moviecatalogservice.models.Movie;
+import com.mufu.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -15,9 +20,27 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userID) {
 
-        return Collections.singletonList(
-                new CatalogItem("Transformers", "good movie", 4)
+        RestTemplate restTemplate = new RestTemplate();
+
+
+        // get all rated movie IDs
+        List<Rating> ratings = Arrays.asList(
+            new Rating("1234", 4),
+            new Rating("5678", 3)
         );
+
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "test movie desc", rating.getRating());
+
+        })
+        .collect(Collectors.toList());
+
+
+        // for each movie ID, call movie info service and get details
+
+        // put them all together
+
 
     }
 
